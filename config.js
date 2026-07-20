1e4d807cbbcdcac03aa66c2e0c651d237041900e
mlmasyan329-del <mlmasyan329@gmail.com>
2026-07-21 05:02:39 +0700

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// ©AiiSigma
-
-module.exports = {
-  domain: "http://server-private.aiisigma.my.id", // Ganti sma domain panel lu dan pastiin harus *http*
-  port: "3002" // Ganti Portnoy di panel network.
-};
